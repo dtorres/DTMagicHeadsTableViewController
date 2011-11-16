899603f420a68785f40c83d4cac3f079f9866372
Diego Torres <warorface@gmail.com>
2011-11-16 16:52:24 -0300

    Initial Commit

---
+++ DTMagicHeadsTableViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DTMagicHeadsTableViewController
+//
+//  Created by Diego Torres on 11/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
